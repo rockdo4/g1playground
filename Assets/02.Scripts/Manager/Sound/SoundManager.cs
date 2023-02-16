6c37eeaefe9db01e990697dcfe8ba801c38b2ff7
유수현 <youg4231998@naver.com>
2023-02-16 14:18:20 +0900

    audio mixer 사용 soundmanager 구현 및 slider 기반 소리 조절 구현

---
+++ Assets/02.Scripts/Manager/Sound/SoundManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] public AudioSource bgm;
+    [SerializeField] public AudioSource se;
+
+    public const string Master_KEY = "MasterVolume";
+    public const string BGM_KEY = "BGMVolume";
+    public const string SE_KEY = "SEVolume";
+
+    private static SoundManager m_instance;
+    public static SoundManager instance
+    {
+        get
+        {
+            if (m_instance == null)
+            {
+                m_instance = FindObjectOfType<SoundManager>();
+            }
+
+            return m_instance;
+        }
+    }
+
+    private void Awake()
+    {
+        //singleton
+        if (instance != this)
+        {
+            
+            Destroy(gameObject);
+        }
+
+        LoadVolume();
+
+    }
+
+    //load volume set on sliders
+    private void LoadVolume()
+    {
+        float masterVolume = PlayerPrefs.GetFloat(Master_KEY, 1f);
+        float bgmVolume = PlayerPrefs.GetFloat(BGM_KEY, 1f);
+        float seVolume = PlayerPrefs.GetFloat(SE_KEY, 1f);
+
+        audioMixer.SetFloat(VolumeSetting.MIXER_Master, Mathf.Log10(masterVolume) * 20);
+        audioMixer.SetFloat(VolumeSetting.MIXER_BGM, Mathf.Log10(bgmVolume) * 20);
+        audioMixer.SetFloat(VolumeSetting.MIXER_SE, Mathf.Log10(seVolume) * 20);
+    }
+}
---
+++ Assets/02.Scripts/Manager/Sound/VolumeSetting.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class VolumeSetting : MonoBehaviour
+{
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] public Slider masterSlider;
+    [SerializeField] public Slider bgmSlider;
+    [SerializeField] public Slider seSlider;
+
+    public const string MIXER_Master = "MasterVolume";
+    public const string MIXER_BGM = "BGMVolume";
+    public const string MIXER_SE = "SEVolume";
+
+    private void Awake()
+    {
+        //add sliders set volume methods
+        masterSlider.onValueChanged.AddListener(SetMasterVolume);
+        bgmSlider.onValueChanged.AddListener(SetBgmVolume);
+        seSlider.onValueChanged.AddListener(SetSEVolume);
+    }
+
+    private void Start()
+    {
+        //Init slider value
+        InitSliderValue();
+    }
+
+    private void OnDisable()
+    {
+        PlayerPrefs.SetFloat(SoundManager.Master_KEY, masterSlider.value);
+        PlayerPrefs.SetFloat(SoundManager.BGM_KEY, bgmSlider.value);
+        PlayerPrefs.SetFloat(SoundManager.SE_KEY, seSlider.value);
+    }
+
+    //Init slider value
+    private void InitSliderValue()
+    {
+        InitMasterSlider();
+        InitBGMSlider();
+        InitSESlider();
+    }
+
+    private void InitMasterSlider()
+    {
+        if (masterSlider == null)
+        {
+            return;
+        }
+        //set min value
+        //if minvalue is under 0 volume goes back to 100%
+        masterSlider.minValue = 0.0001f;
+        masterSlider.value = PlayerPrefs.GetFloat(SoundManager.Master_KEY, 1f);
+    }
+
+    private void InitBGMSlider()
+    {
+        if (bgmSlider == null)
+        {
+            return;
+        }
+        //set min value
+        //if minvalue is under 0 volume goes back to 100%
+        bgmSlider.minValue = 0.0001f;
+        bgmSlider.value = PlayerPrefs.GetFloat(SoundManager.BGM_KEY, 1f);
+    }
+
+    private void InitSESlider()
+    {
+        if (seSlider == null)
+        {
+            return;
+        }
+        //set min value
+        //if minvalue is under 0 volume goes back to 100%
+        seSlider.minValue = 0.0001f;
+        seSlider.value = PlayerPrefs.GetFloat(SoundManager.SE_KEY, 1f);
+    }
+
+    ////change volumes////
+    public void SetMasterVolume(float value)
+    {
+        audioMixer.SetFloat(MIXER_Master, Mathf.Log10(value) * 20);
+    }
+
+    public void SetBgmVolume(float value)
+    {
+        audioMixer.SetFloat(MIXER_BGM, Mathf.Log10(value) * 20);
+    }
+
+    public void SetSEVolume(float value)
+    {
+        audioMixer.SetFloat(MIXER_SE, Mathf.Log10(value) * 20);
+    }
+    ///////////////////////
+}
