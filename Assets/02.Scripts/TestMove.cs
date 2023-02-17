9155d3b5ef70dad4c0f5d531c4eb8bc21ffcee6c
유수현 <youg4231998@naver.com>
2023-02-17 16:46:08 +0900

    tilemap 임시 및 임시 jump tile

---
+++ Assets/02.Scripts/TestMove.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMove : MonoBehaviour
+{
+    public Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rigidbody.AddForce(new Vector3(5, 0));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigidbody.AddForce(new Vector3(-5, 0));
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.AddForce(new Vector3(0, 10));
+        }
+    }
+}
