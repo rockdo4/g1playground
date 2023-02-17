f7957706ff4f41de64e14be9dcfc46f85b8d7d0d
Han Kuen Sol <kshan9279@gmail.com>
2023-02-17 11:04:03 +0900

    이펙트 매니져 완

---
+++ Assets/02.Scripts/Manager/EffectManager.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class EffectManager : MonoBehaviour
+{
+    public GameObject[] effectPrefabs;
+    private Dictionary<string, Queue<GameObject>> effectPool = new Dictionary<string, Queue<GameObject>>();
+
+    private void Start()
+    {
+        foreach (GameObject effectPrefab in effectPrefabs)
+        {
+            string effectName = effectPrefab.name;
+            effectPool[effectName] = new Queue<GameObject>();
+        }
+    }
+
+    public GameObject GetEffect(string effectName)
+    {
+        GameObject effect = null;
+
+        if (!effectPool.ContainsKey(effectName))
+        {
+#if UNITY_EDITOR
+            Debug.Log("Incorrect String");
+#endif
+            return null;
+        }
+
+        if (effectPool.ContainsKey(effectName) && effectPool[effectName].Count > 0)
+        {
+            effect = effectPool[effectName].Dequeue();
+            effect.SetActive(true);
+        }
+        else
+        {
+            foreach (GameObject effectPrefab in effectPrefabs)
+            {
+                if (effectPrefab.name == effectName)
+                {
+                    effect = Instantiate(effectPrefab);
+                    break;
+                }
+            }
+        }
+
+        return effect;
+    }
+
+
+    public void ReturnEffect(string effectName, GameObject effect)
+    {
+        if (!effectPool.ContainsKey(effectName))
+        {
+#if UNITY_EDITOR
+            Debug.Log("Incorrect String");
+#endif
+            return;
+        }
+
+        effect.SetActive(false);
+        effectPool[effectName].Enqueue(effect);
+    }
+
+
+    public void ReturnEffectOnTime(string effectName, GameObject effect, float destroyTime)
+    {
+        StartCoroutine(DelaySetfalse(effectName, effect, destroyTime));
+    }
+
+    private IEnumerator DelaySetfalse(string effectName, GameObject effect, float destroyTime)
+    {
+        if (!effectPool.ContainsKey(effectName))
+        {
+#if UNITY_EDITOR
+            Debug.Log("Incorrect String");
+#endif
+        }
+
+        yield return new WaitForSeconds(destroyTime);
+
+        effect.SetActive(false);
+        effectPool[effectName].Enqueue(effect);
+    }
+
+}
