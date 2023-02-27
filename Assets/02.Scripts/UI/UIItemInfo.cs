2e9b188eed7f4f0fb4d2d56170bb520559680d22
Hansanghwa93 <bdbd5066@naver.com>
2023-02-27 17:59:56 +0900

    UI 레이아웃 재배치

--- Assets/02.Scripts/UI/UIItemInfo.cs
+++ Assets/02.Scripts/UI/UIItemInfo.cs
@@ -4,34 +4,16 @@ using TMPro;
 
 public class UIItemInfo : MonoBehaviour
 {
-    public Image characterImage;
-    //public Image weaponImage;
-    //public Image armorImage;
+    public Image weaponImage;
     public TextMeshProUGUI itemName;
     public TextMeshProUGUI itemInfo;
-    //public TextMeshProUGUI incdecHp;
-    //public TextMeshProUGUI incdecMp;
-    //public TextMeshProUGUI incdecStr;
-    //public TextMeshProUGUI incdecDex;
-    //public TextMeshProUGUI incdecInt;
-    //public TextMeshProUGUI incdecAtk;
-    //public TextMeshProUGUI incdecDef;
 
     private int incdecValue;
     public void SetEmpty()
     {
-        characterImage.sprite = null;
-        //weaponImage.sprite = null;
-        //armorImage.sprite = null;
+        weaponImage.sprite = null;
         itemName.text = string.Empty;
         itemInfo.text = string.Empty;
-        //incdecHp.text = string.Empty;
-        //incdecMp.text = string.Empty;
-        //incdecStr.text = string.Empty;
-        //incdecDex.text = string.Empty;
-        //incdecInt.text = string.Empty;
-        //incdecAtk.text = string.Empty;
-        //incdecDef.text = string.Empty;
     }
 
     public void Set(ItemData data)
@@ -42,8 +24,7 @@ public class UIItemInfo : MonoBehaviour
             return;
         }
 
-        characterImage.sprite = data.iconSprite;
-        //weaponImage.sprite = data.iconSprite;
+        weaponImage.sprite = data.iconSprite;
         itemName.text = data.name;
         itemInfo.text = data.desc;
     }
