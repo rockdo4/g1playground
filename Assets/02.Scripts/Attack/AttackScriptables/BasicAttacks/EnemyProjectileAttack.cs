75b6e9f22425e6d313abd9335e85fd24d4cb8a06
Han Kuen Sol <kshan9279@gmail.com>
2023-03-03 12:51:47 +0900

    원거리 공격 추가

---
+++ Assets/02.Scripts/Attack/AttackScriptables/BasicAttacks/EnemyProjectileAttack.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+[CreateAssetMenu(fileName = "EnemyProjectileAttack", menuName = "BasicAttack/EnemyProjectileAttack")]
+public class EnemyProjectileAttack : BasicAttack
+{
+    public string projectileId;
+    public float range;
+    public float lifeTime;
+
+    public void Fire(GameObject attacker, Vector3 startPos, Vector3 direction)
+    {
+        var aStat = attacker.GetComponent<Status>();
+        if (aStat == null )
+            return;
+        var projectile = GameManager.instance.projectileManager.Get(projectileId);
+        projectile.OnCollided = ExecuteAttack;
+        projectile.Fire(attacker, startPos, direction, range, lifeTime, false, false);
+    }
+}
