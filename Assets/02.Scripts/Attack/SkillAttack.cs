5d03a280cc1f69f2a625c45399f6abf24bca6a1f
ParkMinWoo <super46789@naver.com>
2023-03-03 09:52:12 +0900

    플레이어 스킬 데미지 계수 적용

--- Assets/02.Scripts/Attack/SkillAttack.cs
+++ Assets/02.Scripts/Attack/SkillAttack.cs
@@ -24,7 +24,7 @@ public class SkillAttack : AttackDefinition
     {
         var criticalChance = attacker.FinalValue.criticalChance + this.criticalChance;
         var isCritical = Random.value < criticalChance;
-        float damage = attacker.FinalValue.skillPower;
+        float damage = attacker.FinalValue.skillPower * damageFigure;
         if (isCritical)
         {
             damage *= (attacker.FinalValue.criticalDamage + this.criticalDamage);  // temporary
