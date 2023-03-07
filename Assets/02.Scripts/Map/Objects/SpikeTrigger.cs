6a2f1428e821223bb6fc90aef0a622598434396b
YouSueHyoun <youg4231998@naver.com>
2023-03-07 19:00:59 +0900

    떨어지는 타일 및 가시 타일(가시 타일 및 점프 타일 플레이어 부터 점프, 공격 함수 필요)

---
+++ Assets/02.Scripts/Map/Objects/SpikeTrigger.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeTrigger : MonoBehaviour
+{
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            animator.SetBool("Attack", true);
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            animator.SetBool("Attack", false);
+        }
+    }
+}
