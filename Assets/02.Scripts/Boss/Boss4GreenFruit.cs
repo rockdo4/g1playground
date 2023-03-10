19362a2d2ec90812cc835b66426dd471add0991f
Han Kuen Sol <kshan9279@gmail.com>
2023-03-10 16:26:44 +0900

    보스 수정

--- Assets/02.Scripts/Boss/Boss4GreenFruit.cs
+++ Assets/02.Scripts/Boss/Boss4GreenFruit.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 using static EnemyController;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class Boss4GreenFruit : BossController
 {
@@ -12,6 +14,7 @@ public class Boss4GreenFruit : BossController
     public GameObject attackBox;
     public GameObject dashBox;
     public GameObject skillPivot;
+    public GameObject indicatorBox;
 
     public BasicAttack meleeAttack;
     public BasicAttack meleeSkill;
@@ -75,6 +78,7 @@ public class Boss4GreenFruit : BossController
                     break;
                 case BossState.Skill:
                     agent.isStopped = true;
+                    agent.velocity = Vector3.zero;
                     rb.isKinematic = false;
                     agent.enabled = false;
                     break;
@@ -101,6 +105,20 @@ public class Boss4GreenFruit : BossController
 
     public void Update()
     {
+        //var pos = transform.position;
+        //pos.y += 1f;
+        //pos.z += indicatorBox.transform.localPosition.z;
+        //Ray ray = new Ray(pos, transform.forward);
+        //RaycastHit hit;
+
+        //var currentScale = indicatorBox.transform.localScale;
+        //var layermask = LayerMask.GetMask("Ground");
+        //if (Physics.Raycast(ray, out hit, 7f, layermask))
+        //{
+        //    Debug.Log("ground");
+        //}
+        //Debug.DrawRay(ray.origin, transform.forward*7, Color.red);
+
         dashCoolTime += Time.deltaTime;
         projectileCoolTime += Time.deltaTime;
 
@@ -160,13 +178,13 @@ public class Boss4GreenFruit : BossController
 
         idleCool += Time.deltaTime;
 
-        if (LookAtTarget())
-            if (idleCool >= idleTime)
-            {
-                idleCool = 0f;
-                State = BossState.Motion;
-                animator.SetTrigger("Motion");
-            }
+        //if (LookAtTarget())
+        if (idleCool >= idleTime)
+        {
+            idleCool = 0f;
+            State = BossState.Motion;
+            animator.SetTrigger("Motion");
+        }
     }
 
     private bool isSkillType = false;
@@ -245,7 +263,7 @@ public class Boss4GreenFruit : BossController
     }
 
     private int motionCount = 0;
-    private int maxMotionCount = 3;
+    private int maxMotionCount = 1;
     private void MotionDone()
     {
         if (motionCount != maxMotionCount)
@@ -266,6 +284,7 @@ public class Boss4GreenFruit : BossController
     {
         ChaeckAttackCount();
         attackBox.active = false;
+        attackBox.GetComponent<BoxCollider>().enabled = false;
         State = BossState.Idle;
     }
     private void DashSkillDone()
@@ -282,15 +301,30 @@ public class Boss4GreenFruit : BossController
     private void Bite()
     {
         attackBox.active = true;
+        attackBox.GetComponent<BoxCollider>().enabled = true;
+
     }
 
     private float dashStopTime = 1f;
     IEnumerator CorStopDash()
     {
         animator.speed = 0f;
+        indicatorBox.active = true;
+        SetIndicatorBoxScale();
+
+        var rot = transform.eulerAngles;
+
+        if (rot.y > 180f && rot.y < 360f)
+            rot.y = 270f;
+        else
+            rot.y = 90f;
+        transform.rotation = Quaternion.Euler(rot);
+
+
         yield return new WaitForSeconds(dashStopTime);
 
         animator.speed = 1f;
+        indicatorBox.active = false;
         dashBox.active = true;
         rb.AddForce(transform.forward * 20f, ForceMode.Impulse);
 
@@ -331,11 +365,55 @@ public class Boss4GreenFruit : BossController
         else
             return false;
     }
+    protected void SetIndicatorBoxScale()
+    {
+        //var pos = transform.position;
+        //pos.y += 1f;
+        //pos.z += indicatorBox.transform.localPosition.z;
+        //Ray ray = new Ray(pos, transform.forward);
+        //RaycastHit hit;
+
+        //var currentScale = indicatorBox.transform.localScale;
+        //var layermask = LayerMask.GetMask("Ground");
+        //if (Physics.Raycast(ray, out hit, 7f, layermask))
+        //{
+        //    Debug.Log("ground");
+        //}
+        //Debug.DrawRay(ray.origin, transform.forward*7, Color.red);
+
+        var centerPos = transform.position;
+        centerPos.y += 1f;
+        //centerPos.z = indicatorBox.transform.localPosition.z;
+        var temp = transform;
+        transform.position = centerPos;
+        Ray ray = new Ray(centerPos, transform.TransformDirection(Vector3.forward));
+        RaycastHit hit;
+
+        var currentScale = indicatorBox.transform.localScale;
+        var layermask = LayerMask.GetMask("Ground");
+        if (Physics.Raycast(ray, out hit, 7f, layermask))
+        {
+            currentScale.z = (hit.distance - 1f) * 0.5f;
+        }
+        else
+        {
+            currentScale.z = 6f * 0.5f;
+        }
+
+        indicatorBox.transform.localScale = currentScale;
+    }
 
     private void OnTriggerEnter(Collider collider)
     {
-        if (collider.gameObject.CompareTag("Player"))
+
+        if (indicatorBox.activeSelf)
+            return;
+
+
+        if (collider.gameObject.CompareTag("Player") && collider.gameObject.GetComponent<ObjectMass>() != null)
         {
+            Debug.Log(State);
+
             if (State == BossState.Attack)
                 meleeAttack.ExecuteAttack(gameObject, player.gameObject, transform.position);
             else if (State == BossState.Skill)
