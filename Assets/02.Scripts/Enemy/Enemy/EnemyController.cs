ff7f8b3ddbf47993b6f68470cf250e592913068b
Han Kuen Sol <kshan9279@gmail.com>
2023-03-13 18:37:37 +0900

    에너미 패턴 작업

--- Assets/02.Scripts/Enemy/Enemy/EnemyController.cs
+++ Assets/02.Scripts/Enemy/Enemy/EnemyController.cs
@@ -7,6 +7,18 @@ using UnityEngine.UI;
 
 public class EnemyController : Enemy
 {
+    [System.Serializable]
+    public class EnemyStateData
+    {
+        public EnemyState state;
+        public float second;
+    }
+
+    [Header("<Only Idle and Patrol>")]
+    [SerializeField]
+    public List<EnemyStateData> EnemyStatePattern = new List<EnemyStateData>();
+    private int curCountPattern;
+    private float patternTime = 0f;
 
     public override EnemyState State
     {
@@ -22,35 +34,30 @@ public class EnemyController : Enemy
             switch (State)
             {
                 case EnemyState.None:
-
-                    break;
-                case EnemyState.Spawn:
-
-                    break;
-                case EnemyState.Motion:
-
+                    agent.isStopped = true;
+                    rb.isKinematic = true;
                     break;
                 case EnemyState.Idle:
-
+                    agent.isStopped = true;
+                    rb.isKinematic = true;
+                    break;
+                case EnemyState.Patrol:
+                    agent.isStopped = false;
+                    rb.isKinematic = true;
                     break;
                 case EnemyState.Chase:
-
                     break;
                 case EnemyState.TakeDamage:
-
                     break;
                 case EnemyState.Attack:
-
-                    break;
-                case EnemyState.Skill:
-
                     break;
                 case EnemyState.Die:
                     break;
             }
+
+            Debug.Log(State);
         }
     }
-
 
 
     protected override void Awake()
@@ -59,7 +66,6 @@ public class EnemyController : Enemy
     }
     protected override void Start()
     {
-
 
     }
     protected override void OnEnable()
@@ -74,14 +80,12 @@ public class EnemyController : Enemy
 
         switch (State)
         {
-            case EnemyState.Spawn:
-                Spawn();
+            case EnemyState.None:
+                None();
                 break;
+
             case EnemyState.Idle:
                 Idle();
-                break;
-            case EnemyState.Motion:
-                Motion();
                 break;
             case EnemyState.Patrol:
                 Patrol();
@@ -92,9 +96,6 @@ public class EnemyController : Enemy
             case EnemyState.Attack:
                 Attack();
                 break;
-            case EnemyState.Skill:
-                Skill();
-                break;
             case EnemyState.TakeDamage:
                 TakeDamage();
                 break;
@@ -102,20 +103,69 @@ public class EnemyController : Enemy
                 Die();
                 break;
         }
+
         animator.SetFloat("Move", agent.velocity.magnitude);
     }
-    protected override void Spawn()
+    //protected override void Spawn()
+    //{
+    //}
+    //protected override void Motion()
+    //{
+    //}
+    protected void None()
     {
-    }
-    protected override void Motion()
-    {
+        SaveFloorLength();
+        State = EnemyStatePattern[0].state;
     }
     protected override void Idle()
     {
 
-    }
+        ChangePattern();
 
-    private bool isSkillType = false;
+
+    }
+    public bool isGoingRight;
+    protected override void Patrol()
+    {
+        ChangePattern();
+
+        if (isGoingRight)
+        {
+            if (Vector3.Distance(transform.position, endPos) < 0.1f)
+            {
+                agent.velocity = Vector3.zero;
+
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(startPos - transform.position).normalized, Time.deltaTime * 10f);
+                Vector3 right = new Vector3(1, 0, 0);
+                //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(right), Time.deltaTime * 10f);
+                if (Quaternion.Angle(transform.rotation, Quaternion.LookRotation(startPos - transform.position).normalized) <= 1f)
+                    isGoingRight = false;
+
+                return;
+            }
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(endPos - transform.position).normalized, Time.deltaTime * 10f);
+            agent.SetDestination(endPos);
+        }
+        else
+        {
+            if (Vector3.Distance(transform.position, startPos) < 0.1f)
+            {
+                agent.velocity = Vector3.zero;
+
+
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(endPos - transform.position).normalized, Time.deltaTime * 10f);
+                Vector3 left = new Vector3(-1, 0, 0);
+                // transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left).normalized, Time.deltaTime * 10f);
+                if (Quaternion.Angle(transform.rotation, Quaternion.LookRotation(endPos - transform.position).normalized) <= 1f)
+                    isGoingRight = true;
+
+                return;
+            }
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(startPos - transform.position).normalized, Time.deltaTime * 10f);
+            agent.SetDestination(startPos);
+        }
+        Debug.Log(isGoingRight);
+    }
     protected override void Chase()
     {
 
@@ -123,6 +173,7 @@ public class EnemyController : Enemy
 
     protected override void Attack()
     {
+
     }
 
     protected override void Skill()
@@ -150,14 +201,35 @@ public class EnemyController : Enemy
         {
             Collider collider = hit.collider;
             floorLength = collider.bounds.size.x;
-            startPos = collider.bounds.center - new Vector3((floorLength / 2), 0, 0);
-            endPos = collider.bounds.center + new Vector3((floorLength / 2), 0, 0);
+            startPos = collider.bounds.center - new Vector3((floorLength / 2), -0.5f, 0);
+            endPos = collider.bounds.center + new Vector3((floorLength / 2), 0.5f, 0);
         }
         else
         {
 #if UNITY_EDITOR
-            Debug.Log("NULL!!");
+            Debug.Log("Arrangement Fail");
 #endif
         }
     }
+    private void ChangePattern()
+    {
+        patternTime += Time.deltaTime;
+
+        if (EnemyStatePattern[curCountPattern].second > patternTime)
+            return;
+
+        patternTime = 0f;
+
+        if (curCountPattern == EnemyStatePattern.Count - 1)
+        {
+            curCountPattern = 0;
+        }
+        else
+        {
+            ++curCountPattern;
+        }
+
+        State = EnemyStatePattern[curCountPattern].state;
+    }
+
 }
