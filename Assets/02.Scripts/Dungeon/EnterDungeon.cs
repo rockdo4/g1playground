79847008b9b9e697a1140e62a9de123cfb145cc8
Hansanghwa93 <bdbd5066@naver.com>
2023-03-16 09:14:25 +0900

    오류 수정

--- Assets/02.Scripts/Dungeon/EnterDungeon.cs
+++ Assets/02.Scripts/Dungeon/EnterDungeon.cs
@@ -21,11 +21,10 @@ public class EnterDungeon : MonoBehaviour
 
     public void DungeonON()
     {
-
         var other = GameObject.FindWithTag("Player");
         dungeon.GetComponent<DungeonManager>().DungeonDay.gameObject.SetActive(true);
         dungeon.transform.Find("Exit").gameObject.SetActive(true);
-        PlayerDataManager.instance.SaveLastPos(MapManager.instance.GetCurrentMapName(), other.transform.position);
+        PlayerDataManager.instance.SaveLastPos(MapManager.instance.GetCurrentMapName(), MapManager.instance.GetCurrentChapterName(),  other.transform.position);
     }
 
     private void OnTriggerEnter(Collider other)
