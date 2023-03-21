3bad18d17a2ffa5fb64f3cc7407cb49f24a0fb49
Hansanghwa93 <bdbd5066@naver.com>
2023-03-21 10:49:08 +0900

    Ui save

--- Assets/02.Scripts/Manager/UIManager.cs
+++ Assets/02.Scripts/Manager/UIManager.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using static Unity.Burst.Intrinsics.X86.Avx;
+using UnityEngine.UIElements;
 
 public class UIManager : MonoBehaviour
 {
     public UIButtonManager btManager;
     public UIPopupManager popupManager;
     public CharacterStatUI charStatUI;
-    [SerializeField]
+    
     private GameObject potion;
     private GameObject skillToggle;
 
@@ -24,16 +27,23 @@ public class UIManager : MonoBehaviour
         popupStack = new Stack<GameObject>();
     }
 
+    public  void CharacterImageSwap()
+    {
+        
+    }
+
     public void PlayerHpBar(int maxhp, int currhp)
     {
         charStatUI.maxHp = maxhp;
         charStatUI.currHp = currhp;
+        charStatUI.SetHPBarValue();
     }
 
     public void PlayerMpBar(int maxMp, int currMp)
     {
         charStatUI.maxMp = maxMp;
         charStatUI.currMp = currMp;
+        charStatUI.SetMPBarValue();
     }
 
     public void AddPopUp(GameObject popup)
@@ -54,5 +64,15 @@ public class UIManager : MonoBehaviour
     public void PotionOff()
     {
         potion.SetActive(false);
+    }
+
+    public  void SkillToggleOn()
+    {
+        skillToggle.SetActive(true);
+    }
+
+    public void SkillToggleOff()
+    {
+        skillToggle.SetActive(false);
     }
 }
