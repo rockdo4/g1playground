1e1fbc1bb06987117a4296895306e4d51523014d
sowsky652 <qudrnjs98@gmail.com>
2023-03-30 11:45:22 +0900

    넉백,스턴 초기화시간 12초 변경,히트이펙트버그수정

--- Assets/02.Scripts/Common/AttackedCC.cs
+++ Assets/02.Scripts/Common/AttackedCC.cs
@@ -18,12 +18,12 @@ public class AttackedCC : MonoBehaviour, IAttackable
     private bool knockBackedOnThisFrame;
     public int maxKBCount = 3;
     private int kBCount = 0;
-    public float kBResistTime = 30f;
+    public float kBResistTime = 12f;
     private float kBResistTimer = 0f;
 
     public int maxStunCount = 3;
     private int stunCount = 0;
-    public float stunResistTime = 30f;
+    public float stunResistTime = 12f;
     private float stunResistTimer = 0f;
 
     private bool onSlowDown;
