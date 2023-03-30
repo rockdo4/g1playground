f849b1f8740c97ab4cf820fd7f979973b939b7ba
Hansanghwa93 <bdbd5066@naver.com>
2023-03-30 19:09:31 +0900

    save

--- Assets/02.Scripts/UI/Common/PopUp/ProfilePopUp.cs
+++ Assets/02.Scripts/UI/Common/PopUp/ProfilePopUp.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class ProfilePopUp : PopupUI
 {
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        UI.Instance.popupPanel.menuSlider.ActiveTrue();
+    }
+
     protected override void OnDisable()
     {
         base.OnDisable();
